a3e05fff68a84dfd6cf13210b49a0c2246553bce
Spencer Chang <spencerc99@gmail.com>
2023-03-06 10:00:42 -0800

    fix head

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,15 @@ export default function Document() {
     <Html data-theme="cmyk">
       <Head lang="en">
         <meta charSet="UTF-8" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400;0,600;1,400&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Special+Elite&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <Main />
